bddbf354a9f032249b810e0f1297aca7fc573ee4
jessi@Mayumi <jessicaikuno2@gmail.com>
2023-02-25 11:03:38 -0400

    first commit

---
+++ Laranjas.java
@@ -0,0 +1,24 @@
+
+public class Laranjas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//**João tem 60 laranjas e gostaria de dividi-las em quantidades 
+		/**iguais com seus amigos 
+		 * com Mateus, Pedro e Lucas. / 
+		 */
+        
+		int laranjas;
+		int amigos;
+		int resultado;
+		
+		laranjas =60;
+		amigos = 4;
+		resultado = laranjas / amigos;
+		
+		System.out.println(" A quantidade de laranjas é: " + resultado);
+		
+		
+	}
+
+}
